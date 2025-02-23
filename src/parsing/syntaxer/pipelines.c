0225c8405f90030d24d3701e3a5c80c4a6c834eb
VICXPTRV <viktoriavorontsova.main@gmail.com>
2025-02-23 10:54:03 +0700

    fix: ls|(), remove extra syntax error messages, remove exit

--- src/parsing/syntaxer/pipelines.c
+++ src/parsing/syntaxer/pipelines.c
@@ -6,7 +6,7 @@
 /*   By: vvoronts <vvoronts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 18:07:35 by vvoronts          #+#    #+#             */
-/*   Updated: 2025/02/15 08:55:08 by vvoronts         ###   ########.fr       */
+/*   Updated: 2025/02/23 10:48:42 by vvoronts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ t_node	*parse_pipeline(t_tok **tok, t_ctx *ctx)
 		right = parse_pipeline(tok, ctx);
 		if (!right)
 		{
-			error(2, ctx, (t_m){"syntax error near unexpected token"});
+			// error(2, ctx, (t_m){"syntax error near unexpected token"});
 			return NULL;
 		}
 		root = init_node(operator->type, operator->lexeme, left, right, ctx);
