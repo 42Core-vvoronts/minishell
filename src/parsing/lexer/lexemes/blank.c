6b95c23cd7f0908728fb1a6e91263bfe369faadc
VICXPTRV <viktoriavorontsova.main@gmail.com>
2025-02-24 16:42:27 +0700

    remove newline in blank

--- src/parsing/lexer/lexemes/blank.c
+++ src/parsing/lexer/lexemes/blank.c
@@ -6,7 +6,7 @@
 /*   By: vvoronts <vvoronts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 19:11:12 by vvoronts          #+#    #+#             */
-/*   Updated: 2025/02/24 09:56:20 by vvoronts         ###   ########.fr       */
+/*   Updated: 2025/02/24 15:45:46 by vvoronts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 bool	is_blank(char *lexeme)
 {
 	if (*lexeme && (is_eqlchar(*lexeme, ' ') || 
-		is_eqlchar(*lexeme, '\t') || is_eqlchar(*lexeme, '\n')))
+		is_eqlchar(*lexeme, '\t')))
 		return true;
 	return false;
 }
