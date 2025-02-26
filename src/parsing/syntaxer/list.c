d930a8595d3e6c3ef0032348c8e77ae5c1816192
VICXPTRV <viktoriavorontsova.main@gmail.com>
2025-02-26 17:49:00 +0700

    added error message to list

--- src/parsing/syntaxer/list.c
+++ src/parsing/syntaxer/list.c
@@ -6,18 +6,11 @@
 /*   By: vvoronts <vvoronts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 18:07:09 by vvoronts          #+#    #+#             */
-/*   Updated: 2025/02/18 09:58:36 by vvoronts         ###   ########.fr       */
+/*   Updated: 2025/02/26 17:45:07 by vvoronts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-bool	is_andor(t_tok *tok)
-{
-	if (!tok)
-		return (0);
-	return (tok->type == AND || tok->type == OR);
-}
 
 /**
  * @brief Synatax rule: <list> ::= <pipeline> {('&&' | '||') <pipeline>}*
@@ -49,11 +42,15 @@ t_node	*parse_list(t_tok **tok, t_ctx *ctx)
 		step_forward(tok);
 		right = parse_pipeline(tok, ctx);
 		if (!right || !left)
-		{
-			error(2, ctx, (t_m){"syntax error near unexpected token", operator->lexeme});
-			return NULL;
-		}
+			return ((t_node *)parserror("syntax", operator->lexeme, 2, ctx));
 		left = init_node(type, operator->lexeme, left, right, ctx);
 	}
 	return left;
+}
+
+bool	is_andor(t_tok *tok)
+{
+	if (!tok)
+		return (0);
+	return (tok->type == AND || tok->type == OR);
 }
